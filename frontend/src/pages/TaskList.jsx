c32696e7b3fc6207c1f007d77eaa66a1b81742cb
IMARA Aimad Eddine <96618971+Aimad-IMARA@users.noreply.github.com>
2025-06-22 12:56:19 +0100

    feat(frontend): fetch and display task list

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react';
+import api from '../api/axios';
+import { Link } from 'react-router-dom';
+
 function TaskList() {
-    return <h2>Task List Page</h2>;
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        api.get('/tasks')
+            .then(res => {
+                setTasks(res.data);
+            })
+            .catch(err => {
+                console.error("Error fetching tasks", err);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>Task List</h2>
+            {tasks.length === 0 ? (
+                <p>No tasks available.</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => (
+                        <li key={task.id}>
+                            <strong>{task.title}</strong> — {task.status} | <Link to={`/edit/${task.id}`}>Edit</Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default TaskList;
