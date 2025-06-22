707ed03785d31e73166141f327e819cd0d477731
IMARA Aimad Eddine <96618971+Aimad-IMARA@users.noreply.github.com>
2025-06-22 13:45:48 +0100

    feat(frontend): add task delete functionality

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -5,15 +5,23 @@ import { Link } from 'react-router-dom';
 function TaskList() {
     const [tasks, setTasks] = useState([]);
 
-    useEffect(() => {
+    const fetchTasks = () => {
         api.get('/tasks')
-            .then(res => {
-                setTasks(res.data);
-            })
-            .catch(err => {
-                console.error("Error fetching tasks", err);
-            });
+            .then(res => setTasks(res.data))
+            .catch(err => console.error("Error fetching tasks", err));
+    };
+
+    useEffect(() => {
+        fetchTasks();
     }, []);
+
+    const handleDelete = (id) => {
+        if (!confirm('Are you sure you want to delete this task?')) return;
+
+        api.delete(`/tasks/${id}`)
+            .then(() => fetchTasks())
+            .catch(err => console.error("Error deleting task", err));
+    }
 
     return (
         <div>
@@ -24,7 +32,7 @@ function TaskList() {
                 <ul>
                     {tasks.map(task => (
                         <li key={task.id}>
-                            <strong>{task.title}</strong> - {task.status} | <Link to={`/edit/${task.id}`}>Edit</Link>
+                            <strong>{task.title}</strong> - {task.status} | <Link to={`/edit/${task.id}`}>Edit</Link> | <button onClick={()=> handleDelete(task.id)}> Delete</button>
                         </li>
                     ))}
                 </ul>
