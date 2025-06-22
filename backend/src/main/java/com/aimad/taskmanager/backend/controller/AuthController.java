c5757c8f1a056fa6b9059134f3d83739f8b20a42
IMARA Aimad Eddine <96618971+Aimad-IMARA@users.noreply.github.com>
2025-06-22 18:32:56 +0100

    feat(backend): add controller for user registration and login

---
+++ backend/src/main/java/com/aimad/taskmanager/backend/controller/AuthController.java
@@ -0,0 +1,49 @@
+package com.aimad.taskmanager.backend.controller;
+
+import com.aimad.taskmanager.backend.dto.UserDto;
+import com.aimad.taskmanager.backend.dto.AuthResponse;
+import com.aimad.taskmanager.backend.dto.UserDto;
+import com.aimad.taskmanager.backend.model.User;
+import com.aimad.taskmanager.backend.service.UserService;
+import com.aimad.taskmanager.backend.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserDto request) {
+        if (userService.existsByUsername(request.getUsername())) {
+            return ResponseEntity.badRequest().body("This username already exists.");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        userService.saveUser(user);
+        return ResponseEntity.ok("User registered successfully.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody UserDto request) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(), request.getPassword()
+                )
+        );
+
+        String token = jwtUtil.generateToken(request.getUsername());
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
---
+++ backend/src/main/java/com/aimad/taskmanager/backend/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.aimad.taskmanager.backend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
